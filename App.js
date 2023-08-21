6e41e4cb4993af03ddb73624c81bf927f2d35bcb
Mathias <mathias.blak@gmail.com>
2023-08-21 19:52:18 +0200

    end

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 35 }}>i wanna be rich! I am better than Benjamin at this</Text>
-      <Button title='hello'></Button>
+       <Image
+        source={require('./assets/maersk2.png')}
+        style={{width: 300, height: 300}}
+      />
+      <Text style={{ fontSize: 20 }}>Welcome to the Mærsk Business Card</Text>
+      <View style={styles.button}>
+      <Button title='Contact Information'>
+      </Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +26,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     Text: 200,
   },
+  button: {
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    backgroundColor: 'lightblue', // Set the background color to white
+    borderWidth: 2,
+    borderColor: 'black', // Add a border color if needed
+  },
+
  
 });
